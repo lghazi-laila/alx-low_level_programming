575f41067ae24da2d1727d3f6da940c201612af8
Laila LGHAZI <laila.lghazi@gmail.com>
2023-03-23 20:24:31 +0000

    Task 2 take 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+  * print_alphabet_x10 - Description:
+  * Description : prints 10 times the alphabet, followed by a new line.
   *
   * return : nothing.
   */
