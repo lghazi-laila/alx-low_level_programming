a53f14077ee96781325cc8d47f2dbabfb74b4199
Laila LGHAZI <laila.lghazi@gmail.com>
2023-04-01 23:32:13 +0000

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string, in reverse, followed by a new l0ine.
+  * @s: input string
+  * Return: vois.
+  */
+
+void print_rev(char *s)
+{
+	int L = 0;
+	int i;
+
+	while (s[L] != '\0')
+		L++;
+
+	for (i = L; i <= 0; i--)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
