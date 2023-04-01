cd1125ef7c07c2251898cfa9b7839dd513a8006e
Laila LGHAZI <laila.lghazi@gmail.com>
2023-04-01 23:48:41 +0000

    Task 4 t6

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,12 @@
 void print_rev(char *s)
 {
 	int L = 0;
+	int i;
 
 	while (s[L] != '\0')
 		L++;
 
-	for (i = L-1; i >= 0; i--)
+	for (i = L - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
