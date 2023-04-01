36300cac4869dce9e1979592e9fb41ba5a71c350
Laila LGHAZI <laila.lghazi@gmail.com>
2023-04-01 16:39:15 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strlen - returns the length of a string.
+  * @s: input character.
+  * Return: length of the string.
+  */
+
+int _strlen(char *s)
+{
+	int L = 0;
+
+	while (*s != '\0')
+	{
+		L++;
+		*s = *(s + 1);
+	}
+	return (L);
+}
+
