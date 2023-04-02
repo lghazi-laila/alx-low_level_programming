226cccbb606eab76e4954caf84f5e22bba28c7ee
Laila LGHAZI <laila.lghazi@gmail.com>
2023-04-02 00:29:40 +0000

    Task 6 t4

--- 0x05-pointers_arrays_strings/6-puts22.c
+++ 0x05-pointers_arrays_strings/6-puts22.c
@@ -9,6 +9,15 @@
 
 void puts2(char *str)
 {
+/**	int i = 0;
+ *
+ *	while (str[i] != '\0')
+ *	{
+ *		_putchar(str[i]);
+ *		i = i + 2;
+ *	}
+ *	_putchar('\n');
+ */
 	int len = 0, i = 0;
 
 	while (str[len] != '\0')
