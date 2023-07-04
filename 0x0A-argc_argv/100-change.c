d5a1bf16c4191beecc0d996a23dce689ebd3ce46
Laila LGHAZI <laila.lghazi@gmail.com>
2023-07-04 23:01:06 +0000

    7 commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -45,12 +45,12 @@ int main(int argc, char **argv)
 		}
 		if (amount % 1 >= 0)
 			coins += amount;
-			printf("%d\n", coins);
-			return (0);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		printf("%d\n", coins);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
