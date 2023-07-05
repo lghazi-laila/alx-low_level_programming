e1b22d2c8c5a8b753e7ac264959eef72500ccc7f
Laila LGHAZI <laila.lghazi@gmail.com>
2023-07-05 22:05:17 +0000

    commit 3

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * get_nodeint_at_index - A function that gets a node of list at specific index
+  * get_nodeint_at_index - function that gets a node of list at specific index
   * @head: A pointer to listint_t structure
   * @index: The index of the node
   * Return: A pointer to node at specified index, or NULL if not exist
